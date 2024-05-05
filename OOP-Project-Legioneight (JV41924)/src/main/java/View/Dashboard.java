fa5aaba68e5b58365d8c04e157a4e714916dec43
Ashfaaq Rifath <ashfaaq.rifath2@gmail.com>
2024-05-05 10:21:32 +0530

    Add files via upload

---
+++ OOP-Project-Legioneight (JV41924)/src/main/java/View/Dashboard.java
@@ -0,0 +1,12 @@
+package View;
+
+import javax.swing.*;
+
+public class Dashboard {
+    private JButton customerOrdersButton;
+    private JButton manageSuppliersButton;
+    private JButton manageInventoryButton;
+    private JButton manageEmployeesButton;
+    private JButton employeeJobsButton;
+    private JButton salesReportButton;
+}
---
+++ OOP-Project-Legioneight (JV41924)/src/main/java/View/OrdersView.java
@@ -0,0 +1,16 @@
+package View;
+
+import javax.swing.*;
+
+public class OrdersView {
+    private JTable table1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JButton addButton;
+    private JButton updateButton;
+    private JButton removeButton;
+    private JButton clearButton;
+}
